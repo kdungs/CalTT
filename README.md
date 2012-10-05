05ba352bfd256887abc37f9922a51085378701dc
Kevin D <kevin@kads24.eu>
2012-10-06 01:01:55 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# CalTT
+Generate a nice timetable from your Google Calendar.
+
+## Warning
+**This code is heavily under development.** Don't expect anything to work yet.
+
+## What's this?
+Google Calendar is a pretty nifty thing you might use every day. However when it comes to planning your weekly schedule in school or university you might want something more timetable-ish. This is where CalTT - short for **Cal**endar **T**ime**T**able - comes into play. Since you probably have already put all your relevant events (lectures, tutorials, etc.) in a seperate calendar (e.g. labeled "University"), using CalTT is easily done in four steps:
+ * Obtain your calendar's ID (looks something like randommixoflettersanddigits@group.calendar.google.com)
+ * Clone this repository
+ * _[Generate an OAuth Key](https://code.google.com/apis/console) (For your own safety at this point… Likely to be improved/removed in future versions.)_
+ * Run `caltt.py` _(At this point there is no HTML output, yet. This is soon to be added, of course.)_
+
+## License
+All of the code in this project, if not stated otherwise, is licensed under the terms of the MIT License.
+
+    The MIT License (MIT)
+    Copyright (c) 2012 Kevin D. (https://github.com/SirCoemgen)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
---
+++ caltt.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import datetime
+import dateutil.parser
+import httplib2
+import json
+import pytz
+import requests
+import sys
+import urllib
+
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+
+storage = Storage('__credentials__')
+credentials = storage.get()
+
+if not credentials:
+    flow = OAuth2WebServerFlow(
+        client_id="<YOUR CLIENT ID>",
+        client_secret="<YOUR CLIENT SECRET>",
+        scope="https://www.googleapis.com/auth/calendar.readonly",
+        redirect_uri="urn:ietf:wg:oauth:2.0:oob"
+    )
+    auth_uri = flow.step1_get_authorize_url()
+
+    print("Please open\n\n{}\n\nand insert the resulting code here.".format(auth_uri))
+    code = raw_input("Code: ")
+    credentials = flow.step2_exchange(code)
+    storage.put(credentials)
+
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+CAL_ID = "<YOUR CALENDER ID>"
+_start = datetime.datetime(2012, 10, 8, tzinfo=pytz.timezone("Europe/Berlin"))
+_end = _start + datetime.timedelta(weeks=1)
+
+REQUEST = "https://www.googleapis.com/calendar/v3/calendars/{}/events?timeMin={}&timeMax={}".format(
+    urllib.quote(CAL_ID),
+    urllib.quote(_start.isoformat()),
+    urllib.quote(_end.isoformat())
+)
+
+response, content = http.request(REQUEST)
+if response.status != 200:
+    print(response)
+    sys.exit(response.status)
+
+calendar = json.loads(content)
+events = calendar['items']
+
+TIMETABLE = {}
+
+for event in events:
+    if event.has_key('recurrence'):
+        start = dateutil.parser.parse(event['start']['dateTime'])
+        end = dateutil.parser.parse(event['end']['dateTime'])
+        TIMETABLE[start] = {
+            'title': event['summary'],
+            'type': event['summary'].split(" ")[-1],
+            'location': event['location'],
+            'description': event['description'],
+            'start': start,
+            'end': end
+        }
+
+for key in sorted(TIMETABLE):
+    print(key)
+    for k, v in TIMETABLE[key].iteritems():
+        print(u"{}:\t{}".format(k, v))
+    print("")
+
+
+
+"""
+# for later
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtWebKit import *
+
+# …
+
+QTapp = QApplication(sys.argv)
+
+QTweb = QWebView()
+QTweb.load(QUrl(auth_uri))
+QTweb.show()
+
+sys.exit(QTapp.exec_())
+"""
