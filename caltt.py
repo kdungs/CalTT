550f159a4f353336e0de658610bddaabd7a3805d
Kevin D <kevin@kads24.eu>
2013-01-25 21:12:48 +0100

    A cleaner approach.

--- caltt.py
+++ caltt.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2.7
 # coding=utf-8
 
+from __future__ import division, print_function
+
+import collections
 import datetime
 import dateutil.parser
 import httplib2
+import itertools
 import json
 import pytz
 import sys
@@ -12,7 +16,8 @@ import urllib
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 
-DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 with open('settings.json') as settings_file:
     SETTINGS = json.load(settings_file)
@@ -28,7 +33,6 @@ if not credentials:
         redirect_uri="urn:ietf:wg:oauth:2.0:oob"
     )
     auth_uri = flow.step1_get_authorize_url()
-
     print("Please open\n\n{}\n\nand insert the resulting code here.".format(auth_uri))
     code = raw_input("Code: ")
     credentials = flow.step2_exchange(code)
@@ -37,13 +41,13 @@ if not credentials:
 http = httplib2.Http()
 http = credentials.authorize(http)
 
-_start = datetime.datetime(2013, 1, 14, tzinfo=pytz.timezone("Europe/Berlin"))
-_end = _start + datetime.timedelta(weeks=1)
+start = datetime.datetime(2013, 1, 14, tzinfo=pytz.timezone("Europe/Berlin"))
+end = start + datetime.timedelta(weeks=1)
 
 REQUEST = "https://www.googleapis.com/calendar/v3/calendars/{}/events?timeMin={}&timeMax={}".format(
     urllib.quote(SETTINGS['cal_id']),
-    urllib.quote(_start.isoformat()),
-    urllib.quote(_end.isoformat())
+    urllib.quote(start.isoformat()),
+    urllib.quote(end.isoformat())
 )
 
 response, content = http.request(REQUEST)
@@ -51,30 +55,23 @@ if response.status != 200:
     print(response)
     sys.exit(response.status)
 
-calendar = json.loads(content)
-events = calendar['items']
+events = json.loads(content)['items']
 
-TIMETABLE = {}
+Event = collections.namedtuple('Event', ['title', 'type', 'location', 'start', 'end'])
+TIMETABLE = collections.defaultdict(list)
+for event in map(
+    lambda _e: Event(
+        _e['summary'],
+        _e['summary'].split(" ")[-1],
+        _e['location'],
+        dateutil.parser.parse(_e['start']['dateTime']),
+        dateutil.parser.parse(_e['end']['dateTime'])
+    ),
+    events
+):
+    TIMETABLE[event.start.weekday()].append(event)
 
-for event in events:
-    if event.has_key('recurrence'):
-        start = dateutil.parser.parse(event['start']['dateTime'])
-        end = dateutil.parser.parse(event['end']['dateTime'])
-        TIMETABLE[start] = {
-            'title': event['summary'],
-            'type': event['summary'].split(" ")[-1],
-            'location': event['location'],
-            'start': start,
-            'end': end
-        }
-        if event.has_key('description'):
-            TIMETABLE[start]['description'] = event['description']
-
-TT_by_day = [[
-    TIMETABLE[x] for x in TIMETABLE.iterkeys() if TIMETABLE[x]["start"].weekday() == day
-] for day in range(5)]
-
-for day in range(5):
+for day in TIMETABLE:
     print(DAYS[day])
-    for event in sorted(TT_by_day[day], key=lambda t: t['start'].time()):
-        print(u"{1} {0}".format(event['title'], event['start'].strftime("%H:%M")))
+    for _e in TIMETABLE[day]:
+        print(u"{1} {0}".format(_e.title, _e.start.strftime("%H:%M")))
